e43df266245f1cd5373afdc0844195ee7ee5b17d
Giovanni <giovanniminoru@gmail.com>
2024-09-03 22:36:10 -0300

    ex003 finalizado

---
+++ ex003/index.mjs
@@ -0,0 +1,7 @@
+import { data } from './data.mjs'
+
+const menorFaturamento = data
+  .filter(item => item.valor > 0) 
+  .reduce((min, item) => item.valor < min ? item.valor : min, data[0].valor);
+
+console.log(`O menor valor de faturamento ocorrido em um dia do mês foi: ${menorFaturamento}`);
